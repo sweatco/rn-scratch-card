caebd6163789982fde8b89f6e93d1c1750e7367f
Sasha Egorov <avegrv@gmail.com>
2023-03-02 11:18:43 +0000

    migrate android to 0.71.3

--- example/android/app/src/main/java/in/sweatco/rnscratchcard/MainApplication.java
+++ example/android/app/src/main/java/in/sweatco/rnscratchcard/MainApplication.java
@@ -2,6 +2,7 @@ package in.sweatco.rnscratchcard;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -25,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for RnScratchCardExample:
+          // Packages that cannot be autolinked yet can be added manually here, for MyReactNativePackage:
           // packages.add(new MyReactNativePackage());
           packages.add(new RnScratchCardPackage());
           return packages;
