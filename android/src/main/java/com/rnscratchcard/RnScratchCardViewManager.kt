b0355b9579da31e857c53bdcbddee59a68ce37bd
Cody Miller <hello@looskie.com>
2024-02-04 19:51:57 -0500

    refactor: init request manager + clean up unneeded casts

--- android/src/main/java/com/rnscratchcard/RnScratchCardViewManager.kt
+++ android/src/main/java/com/rnscratchcard/RnScratchCardViewManager.kt
@@ -1,21 +1,28 @@
 package com.rnscratchcard
 
-import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 import com.facebook.react.bridge.ReadableMap
-import com.facebook.react.bridge.WritableNativeArray
-import com.facebook.react.bridge.WritableNativeMap
 import com.facebook.react.common.MapBuilder
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 import com.rnscratchcard.tools.px
 
-class RnScratchCardViewManager : SimpleViewManager<View>() {
+
+class RnScratchCardViewManager : SimpleViewManager<RNScratchCard>() {
+  private var requestManager: RequestManager? = null;
 
   override fun getName() = "RnScratchCardView"
 
-  override fun createViewInstance(reactContext: ThemedReactContext): View {
+  override fun createViewInstance(reactContext: ThemedReactContext): RNScratchCard {
+    this.requestManager = Glide.with(reactContext)
     return RNScratchCard(reactContext)
+  }
+
+  override fun onAfterUpdateTransaction(view: RNScratchCard) {
+    super.onAfterUpdateTransaction(view)
+    view.revalidate(requestManager)
   }
 
   override fun getExportedCustomDirectEventTypeConstants(): MutableMap<String, Any> {
@@ -25,22 +32,17 @@ class RnScratchCardViewManager : SimpleViewManager<View>() {
   }
 
   @ReactProp(name = "image")
-  fun setImage(view: View, source: ReadableMap) {
-    val array = WritableNativeArray().also {
-      it.pushMap(WritableNativeMap().also {
-        it.putString("uri", source.getString("uri"))
-      })
-    }
-    (view as RNScratchCard).setSource(array)
+  fun setImage(view: RNScratchCard, source: ReadableMap) {
+    view.setSource(source)
   }
 
   @ReactProp(name = "brushWidth")
-  fun setBrushWidth(view: View, source: Double) {
-    (view as RNScratchCard).setBrushWidth(source.toFloat().px)
+  fun setBrushWidth(view: RNScratchCard, source: Double) {
+    view.setBrushWidth(source.toFloat().px)
   }
 
   @ReactProp(name = "onScratch", defaultBoolean = false)
-  fun setOnScratch(view: View, onScratch: Boolean) {
-    (view as RNScratchCard).notifyAboutScratchEnabled = onScratch
+  fun setOnScratch(view: RNScratchCard, onScratch: Boolean) {
+    view.notifyAboutScratchEnabled = onScratch
   }
 }
